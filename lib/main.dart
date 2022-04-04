4deb0317c1e3ddd3a1bd7928a6a90726ba2d42bc
arldimeg <arldimegantara@gmail>
2022-04-04 23:41:48 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: LoginScreen()
+    );
+  }
+}
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // text paling atas
+          const Text('Masuk Aplikasi',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          // text diatas textfield
+          const Text('Selamat Datang, Silahkan Masukan Data',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          // textfield email
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Email',
+              prefixIcon: Icon(Icons.email, color: Colors.black,)
+            ),
+          ),
+          const SizedBox(
+            height: 28.0,
+          ),
+          // textfield password
+          const TextField(
+            obscureText: true,
+            decoration: const InputDecoration(
+                hintText: 'Password',
+                prefixIcon: Icon(Icons.lock, color: Colors.black,)
+            ),
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text(
+            'Tidak ingat password?',
+            style: TextStyle(
+              color: Colors.blue
+            ),
+          ),
+          const SizedBox(
+            height: 88.0,
+          ),
+          // container
+          Container(
+            width: double.infinity,
+            // button
+            child: RawMaterialButton(
+              fillColor: Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+              onPressed: () {},
+              child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
